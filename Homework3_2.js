c473718507441c5ea360f113046d38f738393378
Sujinda Lertkhonsarn <sujinda.lks@gmail.com>
2023-07-29 15:44:06 +0700

    update Homework3_2

--- Homework3_2.js
+++ Homework3_2.js
@@ -1,26 +1,26 @@
-const draw32 = function(n) {
-    let text = '';
-    let numbers = '';
-    for (let i = 1; i <= 2 * n - 1; i++) {
-        if (i <= n) {
-            for (let j = 1; j <= i; j++) {
-              numbers += (i + j - 1);
-            }
-          } else {
-            for (let j = 1; j <= 2 * n - i; j++) {
-              numbers += (n - j + 1);
-            }
-          }
-        let spacesCount = n - numbers.length;
-
-        for (let j = 1; j <= spacesCount / 2; j++) {
-            text += '_';
-        }
-        text += numbers;
-        for (let j = 1; j <= spacesCount / 2; j++) {
-            text += '_';
-        }
-        text += "\n";
+const drawH32 = function(n) {
+  let text = '';
+  let left = n;
+  let right = n;
+  let number = 1
+  for (let row = 1; row <= 2 * n - 1; row++) {
+    for (let col = 1; col <= 2 * n - 1; col++) {
+      if (col >= left && col <= right) {
+        text += number.toString();
+        number++;
+      } else {
+        text += "-";
+      }
+    }
+    if (row < n){
+        left -= 1
+        right += 1
+    }
+    else {
+        left += 1
+        right -= 1
+    }
+    text += "\n";
     }
     console.log(text);
 }
